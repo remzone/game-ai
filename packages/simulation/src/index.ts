04f3cd1147acd4216d172fda5e2eccc58a1739b1
Remzi Eshmatov <155640872+remzone@users.noreply.github.com>
2026-09-19 21:55:22 +0300

    feat: add packages/simulation/src/index.ts

---
+++ packages/simulation/src/index.ts
@@ -0,0 +1,7 @@
+export * from './model.js';
+export * from './rng.js';
+export * from './world.js';
+export * from './systems.js';
+export * from './commands.js';
+export * from './combat.js';
+export * from './director.js';
